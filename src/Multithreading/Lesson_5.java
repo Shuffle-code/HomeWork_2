a2344b9698597850a60387614e827fe6f8e26c74
Evgeniy <malugn-evg-nsk@yandex.ru>
2021-07-25 21:34:22 +0700

    add commit 2

---
+++ src/Multithreading/Lesson_5.java
@@ -0,0 +1,14 @@
+package Multithreading;
+
+public class Lesson_5 {
+    public static void main(String[] args) {
+        System.out.println("Loop 1");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Loop 2");
+        for (int i = 5; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
